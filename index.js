def9718815efe45449cb279ea0b536196e9a8620
Heeji Hyun <wlgmlgus@gmail.com>
2022-01-11 21:45:23 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(freeSample){
   // Write your code here!
+  let result;
+  if (freeSample <= 400){
+    result = 'This one is on me!';
+  } else if (freeSample > 2500) {
+    result = 'No can do.';
+  } else if (freeSample > 2000){
+    result = 'I will gladly take your thirty bucks.';
+  } 
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result;
+  if (city == 'NYC') {
+     result = 'Ok, sounds good.';
+  } else {
+    result = 'No go.';
+  }
+ return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generousTip){
   // Write your code here!
+ 
+ switch (generousTip) {
+    case 'generous':
+     return 'Thank you so much.';
+    break;
+
+    case 'not as generous':
+     return 'Thank you.';
+    break;
+
+    case 'thanks for everything': 
+      return 'Bye.';
+    break;
+  } 
 }
